2e19bb8000716ec461c7172140ee8bcbcb91f1cb
bainternet <admin@bainternet.info>
2012-02-05 12:16:03 +0200

    added search.php and searchform.php

---
+++ searchform.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Archive Template
+ * 
+ * @package: WBootStrap
+ * @file: archive.php
+ * @author Ohad Raz 
+ * @since 0.1
+ */
+ ?>
+ <form action="<?php echo home_url( '/' ); ?>" method="get" class="form-stacked">
+    <fieldset>
+		<div class="clearfix">
+			<div class="input">
+				<input type="text" name="s" id="search" placeholder="Search" value="<?php the_search_query(); ?>" />			
+				<button type="submit" class="btn primary">Search</button>
+			</div>
+        </div>
+    </fieldset>
+</form>
