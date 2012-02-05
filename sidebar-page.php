123bf37a680a72a989518baac8f18bc8e964cc8d
bainternet <admin@bainternet.info>
2012-02-05 11:21:55 +0200

    added page.php sidebar-blog.php and sidebar-page.php

---
+++ sidebar-page.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * SideBar Template
+ * 
+ * @package: WBootStrap
+ * @file: sidebar.php
+ * @author Ohad Raz 
+ * @since 0.1
+ */
+ ?>
+<div class="span4">
+	<div class="row">
+		<div class="span3">
+		<h2>Heading</h2>
+		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		<p><a href="#" class="btn">View details »</a></p>
+		</div>
+		<div class="span3">
+		<h2>Heading</h2>
+		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		<p><a href="#" class="btn">View details »</a></p>
+		</div>
+		<div class="span3">
+		<h2>Heading</h2>
+		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		<p><a href="#" class="btn">View details »</a></p>
+		</div>
+		<div class="span3">
+		<h2>Heading</h2>
+		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		<p><a href="#" class="btn">View details »</a></p>
+		</div>
+	</div>
+</div>
